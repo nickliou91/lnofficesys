7b854826bf97088af810b55ac78cf014d45b4eac
~NiCK~ <nickliou91@gmail.com>
2015-04-09 19:39:29 +0800

    change the gem

--- Gemfile
+++ Gemfile
@@ -36,13 +36,10 @@ group :development, :test do
   gem 'byebug',      '3.4.0'
   gem 'web-console', '2.0.0.beta3'
   gem 'spring',      '1.1.3'
-
-end
-
-group :test do
   gem 'minitest-reporters', '1.0.5'
   gem 'mini_backtrace',     '0.1.3'
   gem 'guard-minitest',     '2.3.1'
+
 end
 
 group :production do
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
 
    config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'sheltered-dusk-1220.herokuapp.com'
+  host = 'boiling-retreat-1078.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
