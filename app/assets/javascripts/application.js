da9c6aec62155e374cd59a43fc3600504a69c646
~NiCK~ <nickliou91@gmail.com>
2015-04-10 10:59:56 +0800

    precompile assest

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,16 +17,7 @@
 //= require bootstrap
 
 
-
-    // <!-- jQuery -->
-    // <script src="js/jquery.js"></script>
-
-    // <!-- Bootstrap Core JavaScript -->
-    // <script src="js/bootstrap.min.js"></script>
-
-    // <!-- Script to Activate the Carousel -->
-    // <script>
-		$(document).ready(function() {
+$(document).ready(function() {
 
     $('.carousel').carousel({
         interval: 5000 //changes the speed
